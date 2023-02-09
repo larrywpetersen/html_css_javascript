1360eaf81eae4d0441da86759618ee9f04b746f0
Larry Petersen <larrywp.142@gmail.com>
2023-02-08 20:15:43 -0700

    played with a different way to do a circular count

--- functions.js
+++ functions.js
@@ -7,4 +7,8 @@ function change_div() {
     div = document.getElementById("div1")
     div.style.backgroundColor = colors[color];
     color = (color + 1) % 3;
+    // color = color + 1
+    // if (color > 2) {
+    //     color = 0;
+    // }
 }
